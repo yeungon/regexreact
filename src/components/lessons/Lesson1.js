4e19aad9cbd4b155d2f0f82ca0159aa2bc5f8692
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-11-14 23:32:40 +0700

    Add link to lesson 2
    format code

--- src/components/lessons/Lesson1.js
+++ src/components/lessons/Lesson1.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export default function Lesson1() {
     return (
@@ -8,6 +9,7 @@ export default function Lesson1() {
                 <div className="title">
                     Lesson 1: An Introduction, and the ABCs
                 </div>
+
                 <div className="body">
                     <p><em>Regular expressions</em> are extremely useful in extracting information from text such as
                         code, log
@@ -15,7 +17,8 @@ export default function Lesson1() {
                         languages,
                         the following lessons and examples will explore the more practical uses of regular expressions
                         so
-                        that you can use them as quickly as possible.</p>
+                        that you can use them as quickly as possible.
+                    </p>
                     <p>The first thing to recognize when using regular expressions is that <em>everything is essentially
                         a
                         character</em>, and we are writing patterns to match a specific sequence of characters (also
@@ -24,16 +27,19 @@ export default function Lesson1() {
                         other
                         symbols on your keyboard like %#$@!, but unicode characters can also be used to match any type
                         of
-                        international text.</p>
+                        international text.
+                    </p>
                     <p>Below are a couple lines of text, notice how the text changes to highlight the matching
                         characters
                         on each line as you type in the input field below. To continue to the next lesson, you will need
                         to use the new syntax and concept introduced in each lesson to write a pattern that matches all
                         the
-                        lines provided.</p>
+                        lines provided.
+                    </p>
                     <p>Go ahead and try writing a pattern that matches all three rows, <em>it may be as simple as the
                         common
-                        letters on each line</em>.</p>
+                        letters on each line</em>.
+                    </p>
                 </div>
             </div>
             {/* PROBLEMS */}
@@ -65,7 +71,8 @@ export default function Lesson1() {
                     </tr>
                     </tbody>
                 </table>
-                <div className="input_and_continue row" style={{margin: 0}}>
+
+                <div className="input_and_continue row" style={{ margin: 0 }}>
                     <form action="/lesson/letters_and_digits" method="get">
                         <input className="exercise_input_field col-xs-12 col-sm-9 col-md-9" type="text"
                                autoCapitalize="off" autoComplete="off" autoCorrect="off" spellCheck="false"
@@ -74,29 +81,34 @@ export default function Lesson1() {
                                defaultValue="Continue ›"/>
                     </form>
                 </div>
-                <div className="row" style={{margin: 0}}>
+
+                <div className="row" style={{ margin: 0 }}>
                     <div className="solution col-xs-12">
                         <table>
                             <tbody>
                             <tr>
                                 <td className="title">Solution</td>
-                                <td><p>Try typing the first three characters <a className="editor_val"
-                                                                                href="#">abc</a> to see them match
-                                    all the lines.</p></td>
+                                <td>
+                                    <p>Try typing the first three characters
+                                        <a className="editor_val" href="#">abc</a> to see them match all the lines.
+                                    </p>
+                                </td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
+
                 <div className="solution_hint">
-                    Solve the above task to continue on to the next problem, or read the <a
-                    className="show_solution_button" href="#">Solution</a>.
+                    Solve the above task to continue on to the next problem, or read the
+                    <a className="show_solution_button" href="#">Solution</a>.
                 </div>
             </div>
             {/* FOOTER */}
             <div className="footer col-xs-12 col-sm-12 col-md-6">
-                Next – <a href="/lesson/letters_and_digits" title="Lesson 1½: The 123s">Lesson 1½: The 123s</a><br/>
+                Next – <Link to="/lesson/letters_and_digits" title="Lesson 1½: The 123s">Lesson 1½: The 123s</Link><br/>
             </div>
+
             <div className="footer ralign col-xs-12 col-md-6">
                 Find RegexOne useful? Please consider <br/>
                 <a className="paypal_donation_button" href="#" title="Paypal donation">Donating ($4) via Paypal</a> to
