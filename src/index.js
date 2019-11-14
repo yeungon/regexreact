bc1e013f7ad5b0c5cf47ae4d79e175510667f9bb
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-11-14 22:55:14 +0700

    Add route homepage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+    BrowserRouter as Router,
+    Route
+} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+render(
+    <Router>
+        <Route path="/" component={App}/>
+    </Router>,
+    rootEl
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
