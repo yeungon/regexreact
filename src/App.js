ca7e6cc258869e44dbe26a8c7e28092f45bb0d56
Vuong Nguyen <yeungon@gmail.com>
2019-11-14 22:32:26 -0600

    Creating the component 2 - Adding the Switch to the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import {
+    Route,
+    NavLink, 
+    Switch,
+    Link 
+    
+} from "react-router-dom";
+
 import Headers from './components/layouts/Headers';
 import Menu from './components/layouts/Menu';
 import Notes from './components/layouts/Notes';
 import Footers from './components/layouts/Footers';
 
 import Lesson1 from './components/lessons/Lesson1';
+import Lesson2 from './components/lessons/Lesson2';
 
 export default function App() {
     return (
@@ -20,7 +29,16 @@ export default function App() {
                     {/* LESSON */}
                     <div className="row">
                         <div className="col-xs-12 col-sm-12 col-md-9">
-                            <Lesson1/>
+                            
+                        <Switch>
+                            <Route exact path="/" component={Lesson1} />
+
+                            <Route exact path="/lesson/introduction_abcs" component={Lesson1} />
+                            
+                            {<Route path="/lesson/letters_and_digits" component={Lesson2} /> }
+                            
+                        </Switch>
+                            
                         </div>
                         {/* NOTES */}
                         <Notes/>
---
+++ src/components/lessons/Lesson2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export default function Lesson2(){
+
+	return (
+			<div>
+			"Hello component 2"
+			</div>
+		);
+}
